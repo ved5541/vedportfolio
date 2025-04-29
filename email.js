b195d4a99fd861c468fa4dbf3be785620e758e50
ved kumar singh <114964330+ved5541@users.noreply.github.com>
2025-04-28 19:18:45 -0700

    Add files via upload

---
+++ email.js
@@ -0,0 +1,35 @@
+function sendMail(){
+    var params={
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        mobile:document.getElementById("mobile").value,
+        subject:document.getElementById("subject").value,
+        message:document.getElementById("message").value,
+
+
+    };
+    const serviceID ="service_gnqy4al";
+    const templateID = "template_xgykewd";
+    emailjs.send(serviceID,templateID,params)
+.then(
+    res =>{
+        document.getElementById("name").value="";
+        document.getElementById("email").value="";
+        document.getElementById("mobile").value="";
+        document.getElementById("subject").value="";
+        document.getElementById("message").value="";
+
+        console.log(res);
+        alert("your message sent sucessfully");
+
+
+
+
+
+
+    }
+)
+.catch((err)=> console.log(err));
+}
+
+
